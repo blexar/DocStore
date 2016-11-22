1605bd548d6d6f181c67a76b9f146027ab8df9b8
gituser <gituser@me.it>
2016-11-22 21:18:42 +0100

    changes to comments

--- app.js
+++ app.js
@@ -2,16 +2,15 @@ var express = require('express');
 var redis = require('redis');
 var bodyParser=require('body-parser');
 const uuid = require('uuid');
-var tools=require('./test-node');
 
-// inizializza express
+// init express
 var app=express();
 
 app.use(bodyParser.json());
 
 const PORT=4158;
 
-// attiva Redis e controllo errore
+// initialize the Redis client and check if errors occur
 var client = redis.createClient();
 
 client.on('error', function (err) {
@@ -34,12 +33,12 @@ app.post('/docstore/add', function (req, res) {
         {
             if (reply==1)
             {
-               // chiave esiste davvero
+               // the key exists
                dbname=req.headers["doc-store-id"];
             }
             else
             {
-              // chiave cercata non esiste
+              // the key doesn't exist
               res.status(404).end();
             }
         });
@@ -59,60 +58,12 @@ app.post('/docstore/add', function (req, res) {
        }
     });
 
-    //console.log("Accodo "+newdoc+" a "+newdb);
-    //client.lpush(dbname, newdoc);
-
 
 });
 
-/*app.post('/elenco/crea/:quanti', function (req, res) {
-	db.gestionePersone.creaCasuali(req.params.quanti);
-	res.end();
-});
- 
-app.get('/elenco/tutti', function (req, res) {
-	res.send(db.gestionePersone.tutti());
-});*/
 
-client.exists("eb9c93b1-b0c2-11e6-97e8-df10cc6789f2", function(err, reply)
-{
-  console.log('');
-});
 app.listen(PORT, function () {
-  console.log('Server in esecuzione sulla porta '+PORT+'...');
+  console.log('Server running on '+PORT+' port...');
 });
-
-// ********** prove per app
-
-// 1. inserisci un documento
-
-/*newdb=uuid.v1();
-
-obs=[{"nome":"Carlo", "cognome":"Bianchi", "eta":25},
-{"nome":"Carlo", "cognome":"Bianchi", "eta":25},
-{"nome":"Carlo", "cognome":"Bianchi", "eta":25},
-{"nome":"Carlo", "cognome":"Bianchi", "eta":25},
-{"nome":"Carlo", "cognome":"Bianchi", "eta":25}];
-
-obs.forEach(function(entry) {
-    newdoc=uuid.v1();
-    console.log("Inserisco "+entry+" in "+newdoc);
-    client.hmset(newdoc, entry);
-
-    console.log("Accodo "+newdoc+" a "+newdb);
-    client.lpush(newdb, newdoc);
-});
-
-// recupera terzo elemento
-
-client.lrange(newdb,2,2, function(err, reply) {
-    console.log(reply[0]);
-    client.hgetall(reply[0], function(err, r)
-    {
-      console.log(r['nome']);
-    });
-    
-});*/
-
 
 
